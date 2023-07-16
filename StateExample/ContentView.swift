233c4c156c1ece5c09faed29cb921b6ba777ebd6
Pac12Run8000 <gangstarr78992@>
2023-07-16 16:38:56 -0700

    Added functionality for @State

--- StateExample/ContentView.swift
+++ StateExample/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Create a @State property for the toggle
+    @State private var isToggleOn: Bool = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Toggle(isOn: $isToggleOn) {
+                Text("Toggle")
+            }
+            .padding()
+
+            if isToggleOn {
+                Text("The toggle is on")
+            } else {
+                Text("The toggle is off")
+            }
         }
-        .padding()
     }
 }
 
